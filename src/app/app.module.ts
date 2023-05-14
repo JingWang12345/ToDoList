0ed355d343afb2d3dc67af9b6192ee23f1d78fc9
JingWang <wangjing061993@gmail.com>
2023-05-14 18:53:40 +0200

    folderchange

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './component/todos/todos.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoAddComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
